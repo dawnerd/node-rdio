fd62ad77793eca59562b405faaebf6419b3dce99
Troy Whiteley <dawnerd@gmail.com>
2011-06-22 23:33:15 -0700

    Initial commit.
    Note: Test is currently a WIP.

---
+++ rdio.js
@@ -0,0 +1,56 @@
+/***
+ *                                           
+ *                  |                 |o     
+ *    ,---.,---.,---|,---.   ,---.,---|.,---.
+ *    |   ||   ||   ||---'---|    |   |||   |
+ *    `   '`---'`---'`---'   `    `---'``---'
+ *                                           
+ *	Author: Troy Whiteley <troy@somanyscientists.com>
+ *	Homepage: http://github.com/dawnerd/
+ *	License: CC BY-SA 3.0 (http://creativecommons.org/licenses/by-sa/3.0/)
+ *
+ *
+ *	Config options:
+ *		rdio_api_key
+ *		rdio_api_shared
+ *		callback_url - URL user will be redirected to upon login
+ */
+
+module.exports = function(config, oauth) {
+	if(typeof oauth == 'undefined') {
+		var oauth = require('oauth').OAuth;
+	}
+	
+	config.rdio_oauth_request = 'http://api.rdio.com/oauth/request_token';
+	config.rdio_oauth_access = 'http://api.rdio.com/oauth/access_token';
+	config.rdio_oauth_auth = 'https://www.rdio.com/oauth/authorize?oauth_token=';
+	config.rdio_api = 'http://api.rdio.com/1/';
+	
+	//setup oauth
+	var oa = new OAuth(
+		config.rdio_oauth_request,
+		config.rdio_oauth_access, 
+		config.rdio_api_key,
+		config.rdio_api_shared, 
+		"1.0", config.callback_url, "HMAC-SHA1");
+	
+	//public methods	
+	return {
+		getRequestToken: function(callback) {
+			oa.getOAuthRequestToken(callback);
+		},
+		getAccessToken: function(auth_token, auth_token_secret, oauth_verifier, callback) {
+			oa.getOAuthAccessToken(auth_token, auth_token_secret, oauth_verifier, callback);
+		},
+		api: function(auth_token, auth_token_secret, data, callback) {
+			oa.post(
+				config.rdio_api,
+				auth_token,
+				auth_token_secret,
+				data,
+				null,
+				callback
+			);
+		}
+	};
+};
